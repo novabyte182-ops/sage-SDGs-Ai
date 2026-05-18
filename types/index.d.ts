a3680f509790f8033af1fab2f773033ad070a45a
novabyte182-ops <novabyte182@gmail.com>
2026-05-18 07:38:22 -0700

    Add files via upload

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,97 +1,337 @@
-export { Surface } from './container/Surface';
-export type { Props as SurfaceProps } from './container/Surface';
-export { Layer } from './container/Layer';
-export type { Props as LayerProps } from './container/Layer';
-export { Legend } from './component/Legend';
-export type { Props as LegendProps } from './component/Legend';
-export { DefaultLegendContent } from './component/DefaultLegendContent';
-export type { Props as DefaultLegendContentProps } from './component/DefaultLegendContent';
-export { Tooltip } from './component/Tooltip';
-export type { TooltipProps } from './component/Tooltip';
-export { DefaultTooltipContent } from './component/DefaultTooltipContent';
-export type { Props as DefaultTooltipContentProps } from './component/DefaultTooltipContent';
-export { ResponsiveContainer } from './component/ResponsiveContainer';
-export type { Props as ResponsiveContainerProps } from './component/ResponsiveContainer';
-export { Cell } from './component/Cell';
-export type { Props as CellProps } from './component/Cell';
-export { Text } from './component/Text';
-export type { Props as TextProps } from './component/Text';
-export { Label } from './component/Label';
-export type { Props as LabelProps } from './component/Label';
-export { LabelList } from './component/LabelList';
-export type { Props as LabelListProps } from './component/LabelList';
-export { Customized } from './component/Customized';
-export type { Props as CustomizedProps } from './component/Customized';
-export { Sector } from './shape/Sector';
-export type { Props as SectorProps } from './shape/Sector';
-export { Curve } from './shape/Curve';
-export type { Props as CurveProps } from './shape/Curve';
-export { Rectangle } from './shape/Rectangle';
-export type { Props as RectangleProps } from './shape/Rectangle';
-export { Polygon } from './shape/Polygon';
-export type { Props as PolygonProps } from './shape/Polygon';
-export { Dot } from './shape/Dot';
-export type { Props as DotProps } from './shape/Dot';
-export { Cross } from './shape/Cross';
-export type { Props as CrossProps } from './shape/Cross';
-export { Symbols } from './shape/Symbols';
-export type { SymbolsProps } from './shape/Symbols';
-export { PolarGrid } from './polar/PolarGrid';
-export type { Props as PolarGridProps } from './polar/PolarGrid';
-export { PolarRadiusAxis } from './polar/PolarRadiusAxis';
-export type { Props as PolarRadiusAxisProps } from './polar/PolarRadiusAxis';
-export { PolarAngleAxis } from './polar/PolarAngleAxis';
-export type { Props as PolarAngleAxisProps } from './polar/PolarAngleAxis';
-export { Pie } from './polar/Pie';
-export type { Props as PieProps, PieLabel, PieLabelRenderProps } from './polar/Pie';
-export { Radar } from './polar/Radar';
-export type { Props as RadarProps } from './polar/Radar';
-export { RadialBar } from './polar/RadialBar';
-export type { RadialBarProps } from './polar/RadialBar';
-export { Brush } from './cartesian/Brush';
-export type { Props as BrushProps } from './cartesian/Brush';
-export { ReferenceLine } from './cartesian/ReferenceLine';
-export type { Props as ReferenceLineProps } from './cartesian/ReferenceLine';
-export { ReferenceDot } from './cartesian/ReferenceDot';
-export type { Props as ReferenceDotProps } from './cartesian/ReferenceDot';
-export { ReferenceArea } from './cartesian/ReferenceArea';
-export type { Props as ReferenceAreaProps } from './cartesian/ReferenceArea';
-export { CartesianAxis } from './cartesian/CartesianAxis';
-export type { Props as CartesianAxisProps } from './cartesian/CartesianAxis';
-export { CartesianGrid } from './cartesian/CartesianGrid';
-export type { Props as CartesianGridProps } from './cartesian/CartesianGrid';
-export { Line } from './cartesian/Line';
-export type { Props as LineProps } from './cartesian/Line';
-export { Area } from './cartesian/Area';
-export type { Props as AreaProps } from './cartesian/Area';
-export { Bar } from './cartesian/Bar';
-export type { Props as BarProps } from './cartesian/Bar';
-export { Scatter } from './cartesian/Scatter';
-export type { Props as ScatterProps } from './cartesian/Scatter';
-export { XAxis } from './cartesian/XAxis';
-export type { Props as XAxisProps } from './cartesian/XAxis';
-export { YAxis } from './cartesian/YAxis';
-export type { Props as YAxisProps } from './cartesian/YAxis';
-export { ZAxis } from './cartesian/ZAxis';
-export type { Props as ZAxisProps } from './cartesian/ZAxis';
-export { ErrorBar } from './cartesian/ErrorBar';
-export type { Props as ErrorBarProps } from './cartesian/ErrorBar';
-export { LineChart } from './chart/LineChart';
-export { BarChart } from './chart/BarChart';
-export { PieChart } from './chart/PieChart';
-export { Treemap } from './chart/Treemap';
-export type { Props as TreemapProps } from './chart/Treemap';
-export { Sankey } from './chart/Sankey';
-export { RadarChart } from './chart/RadarChart';
-export { ScatterChart } from './chart/ScatterChart';
-export { AreaChart } from './chart/AreaChart';
-export { RadialBarChart } from './chart/RadialBarChart';
-export { ComposedChart } from './chart/ComposedChart';
-export { SunburstChart } from './chart/SunburstChart';
-export { Funnel } from './numberAxis/Funnel';
-export type { FunnelProps } from './numberAxis/Funnel';
-export { FunnelChart } from './chart/FunnelChart';
-export { Trapezoid } from './shape/Trapezoid';
-export type { Props as TrapezoidProps } from './shape/Trapezoid';
-export { Global } from './util/Global';
-export type { LegendType } from './util/types';
+/// <reference types="node" />
+/// <reference types="react" />
+/// <reference types="react/experimental" />
+/// <reference types="react-dom" />
+/// <reference types="react-dom/experimental" />
+
+import type { Agent as HttpAgent } from 'http'
+import type { Agent as HttpsAgent } from 'https'
+
+import type React from 'react'
+import type { ParsedUrlQuery } from 'querystring'
+import type { IncomingMessage, ServerResponse } from 'http'
+
+import {
+  NextPageContext,
+  NextComponentType,
+  NextApiResponse,
+  NextApiRequest,
+  NextApiHandler,
+  // @ts-ignore This path is generated at build time and conflicts otherwise
+} from '../dist/shared/lib/utils'
+
+import type {
+  NextApiRequestCookies,
+  // @ts-ignore This path is generated at build time and conflicts otherwise
+} from '../dist/server/api-utils'
+
+// @ts-ignore This path is generated at build time and conflicts otherwise
+import next from '../dist/server/next'
+
+export type ServerRuntime = 'nodejs' | 'experimental-edge' | 'edge' | undefined
+
+// @ts-ignore This path is generated at build time and conflicts otherwise
+export { NextConfig } from '../dist/server/config'
+
+export type {
+  Metadata,
+  MetadataRoute,
+  ResolvedMetadata,
+  ResolvingMetadata,
+  Viewport,
+  ResolvingViewport,
+  ResolvedViewport,
+  // @ts-ignore This path is generated at build time and conflicts otherwise
+} from '../dist/lib/metadata/types/metadata-interface'
+
+/**
+ * Stub route type for typedRoutes before `next dev` or `next build` is run
+ * @link https://nextjs.org/docs/app/building-your-application/configuring/typescript#statically-typed-links
+ * @example
+ * ```ts
+ * import type { Route } from 'next'
+ * // ...
+ * router.push(returnToPath as Route)
+ * ```
+ */
+
+// `RouteInferType` is a stub here to avoid breaking `typedRoutes` when the type
+// isn't generated yet. It will be replaced when the webpack plugin runs.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export type Route<RouteInferType = any> = string & {}
+
+// Extend the React types with missing properties
+declare module 'react' {
+  // <html amp=""> support
+  interface HtmlHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    amp?: string
+  }
+
+  // <img fetchPriority=""> support
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- It's actually required for module augmentation to work.
+  interface ImgHTMLAttributes<T> {
+    fetchPriority?: 'high' | 'low' | 'auto' | undefined
+  }
+}
+
+export type Redirect =
+  | {
+      statusCode: 301 | 302 | 303 | 307 | 308
+      destination: string
+      basePath?: false
+    }
+  | {
+      permanent: boolean
+      destination: string
+      basePath?: false
+    }
+
+/**
+ * `NextPage` type, use it as a guide to create `pages`.
+ */
+export type NextPage<Props = {}, InitialProps = Props> = NextComponentType<
+  NextPageContext,
+  InitialProps,
+  Props
+>
+
+export type FileSizeSuffix = `${
+  | 'k'
+  | 'K'
+  | 'm'
+  | 'M'
+  | 'g'
+  | 'G'
+  | 't'
+  | 'T'
+  | 'p'
+  | 'P'}${'b' | 'B'}`
+
+export type SizeLimit = number | `${number}${FileSizeSuffix}`
+
+export type ResponseLimit = SizeLimit | boolean
+
+/**
+ * `Config` type, use it for export const config
+ */
+export type PageConfig = {
+  amp?: boolean | 'hybrid'
+  api?: {
+    /**
+     * Configures or disables body size limit warning. Can take a number or
+     * any string format supported by `bytes`, for example `1000`, `'500kb'` or
+     * `'3mb'`.
+     */
+    responseLimit?: ResponseLimit
+    /**
+     * The byte limit of the body. This is the number of bytes or any string
+     * format supported by `bytes`, for example `1000`, `'500kb'` or `'3mb'`.
+     */
+    bodyParser?:
+      | {
+          sizeLimit?: SizeLimit
+        }
+      | false
+    /**
+     * Flag to disable warning "API page resolved
+     * without sending a response", due to explicitly
+     * using an external API resolver, like express
+     */
+    externalResolver?: true
+  }
+  env?: Array<string>
+  /**
+   * Configures the longest time in seconds a serverless function can process an HTTP
+   * request before responding.
+   */
+  maxDuration?: number
+  runtime?: ServerRuntime
+  unstable_runtimeJS?: false
+  unstable_JsPreload?: false
+  /**
+   * @deprecated this config has been removed in favor of the next.config.js option
+   */
+  // TODO: remove in next minor release (current v13.1.1)
+  unstable_includeFiles?: string[]
+  /**
+   * @deprecated this config has been removed in favor of the next.config.js option
+   */
+  // TODO: remove in next minor release (current v13.1.1)
+  unstable_excludeFiles?: string[]
+}
+
+export {
+  NextPageContext,
+  NextComponentType,
+  NextApiResponse,
+  NextApiRequest,
+  NextApiHandler,
+}
+
+export type PreviewData = string | false | object | undefined
+
+/**
+ * Context object passed into `getStaticProps`.
+ * @link https://nextjs.org/docs/api-reference/data-fetching/get-static-props#context-parameter
+ */
+export type GetStaticPropsContext<
+  Params extends ParsedUrlQuery = ParsedUrlQuery,
+  Preview extends PreviewData = PreviewData
+> = {
+  params?: Params
+  preview?: boolean
+  previewData?: Preview
+  draftMode?: boolean
+  locale?: string
+  locales?: string[]
+  defaultLocale?: string
+  revalidateReason?: 'on-demand' | 'build' | 'stale'
+}
+
+/**
+ * The return type of `getStaticProps`.
+ * @link https://nextjs.org/docs/api-reference/data-fetching/get-static-props#getstaticprops-return-values
+ */
+export type GetStaticPropsResult<Props> =
+  | { props: Props; revalidate?: number | boolean }
+  | { redirect: Redirect; revalidate?: number | boolean }
+  | { notFound: true; revalidate?: number | boolean }
+
+/**
+ * Static Site Generation feature for Next.js.
+ * @link https://nextjs.org/docs/basic-features/data-fetching/get-static-props
+ * @link https://nextjs.org/docs/basic-features/typescript#static-generation-and-server-side-rendering
+ * @example
+ * ```ts
+ * export const getStaticProps: GetStaticProps = async (context) => {
+ *   // ...
+ * }
+ * ```
+ */
+export type GetStaticProps<
+  Props extends { [key: string]: any } = { [key: string]: any },
+  Params extends ParsedUrlQuery = ParsedUrlQuery,
+  Preview extends PreviewData = PreviewData
+> = (
+  context: GetStaticPropsContext<Params, Preview>
+) => Promise<GetStaticPropsResult<Props>> | GetStaticPropsResult<Props>
+
+export type InferGetStaticPropsType<T extends (args: any) => any> = Extract<
+  Awaited<ReturnType<T>>,
+  { props: any }
+>['props']
+
+export type GetStaticPathsContext = {
+  locales?: string[]
+  defaultLocale?: string
+}
+
+/**
+ * The return type of `getStaticPaths`.
+ * @link https://nextjs.org/docs/api-reference/data-fetching/get-static-paths#getstaticpaths-return-values
+ */
+export type GetStaticPathsResult<
+  Params extends ParsedUrlQuery = ParsedUrlQuery
+> = {
+  paths: Array<string | { params: Params; locale?: string }>
+  fallback: boolean | 'blocking'
+}
+
+/**
+ * Define a list of paths to be statically generated if dynamic routes exist.
+ * @link https://nextjs.org/docs/basic-features/data-fetching/get-static-paths
+ * @link https://nextjs.org/docs/basic-features/typescript#static-generation-and-server-side-rendering
+ * @example
+ * ```ts
+ * export const getStaticPaths: GetStaticPaths = async () => {
+ *  // ...
+ * }
+ * ```
+ */
+export type GetStaticPaths<Params extends ParsedUrlQuery = ParsedUrlQuery> = (
+  context: GetStaticPathsContext
+) => Promise<GetStaticPathsResult<Params>> | GetStaticPathsResult<Params>
+
+/**
+ * Context object passed into `getServerSideProps`.
+ * @link https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props#context-parameter
+ */
+export type GetServerSidePropsContext<
+  Params extends ParsedUrlQuery = ParsedUrlQuery,
+  Preview extends PreviewData = PreviewData
+> = {
+  req: IncomingMessage & {
+    cookies: NextApiRequestCookies
+  }
+  res: ServerResponse
+  params?: Params
+  query: ParsedUrlQuery
+  preview?: boolean
+  previewData?: Preview
+  draftMode?: boolean
+  resolvedUrl: string
+  locale?: string
+  locales?: string[]
+  defaultLocale?: string
+}
+
+/**
+ * The return type of `getServerSideProps`.
+ * @link https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props#getserversideprops-return-values
+ */
+export type GetServerSidePropsResult<Props> =
+  | { props: Props | Promise<Props> }
+  | { redirect: Redirect }
+  | { notFound: true }
+
+/**
+ * Server-side Rendering feature for Next.js.
+ * @link https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props
+ * @link https://nextjs.org/docs/basic-features/typescript#static-generation-and-server-side-rendering
+ * @example
+ * ```ts
+ * export const getServerSideProps: GetServerSideProps = async (context) => {
+ *  // ...
+ * }
+ */
+export type GetServerSideProps<
+  Props extends { [key: string]: any } = { [key: string]: any },
+  Params extends ParsedUrlQuery = ParsedUrlQuery,
+  Preview extends PreviewData = PreviewData
+> = (
+  context: GetServerSidePropsContext<Params, Preview>
+) => Promise<GetServerSidePropsResult<Props>>
+
+export type InferGetServerSidePropsType<T extends (args: any) => any> = Awaited<
+  Extract<Awaited<ReturnType<T>>, { props: any }>['props']
+>
+
+declare global {
+  interface Crypto {
+    readonly subtle: SubtleCrypto
+    getRandomValues<
+      T extends
+        | Int8Array
+        | Int16Array
+        | Int32Array
+        | Uint8Array
+        | Uint16Array
+        | Uint32Array
+        | Uint8ClampedArray
+        | Float32Array
+        | Float64Array
+        | DataView
+        | null
+    >(
+      array: T
+    ): T
+    randomUUID(): string
+  }
+
+  var __NEXT_HTTP_AGENT_OPTIONS: { keepAlive?: boolean } | undefined
+  var __NEXT_UNDICI_AGENT_SET: boolean
+  var __NEXT_HTTP_AGENT: HttpAgent
+  var __NEXT_HTTPS_AGENT: HttpsAgent
+}
+
+export default next
